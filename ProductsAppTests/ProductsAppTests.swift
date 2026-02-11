f6b38b8206a4d67dfe5641e3f75864b646c3df30
Ted Philip Lat <ted.lat@maya.ph>
2026-02-11 16:56:33 +0800

    Initial Commit

---
+++ ProductsAppTests/ProductsAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProductsAppTests.swift
+//  ProductsAppTests
+//
+//  Created by Ted Philip Lat on 2/11/26.
+//
+
+import Testing
+@testable import ProductsApp
+
+struct ProductsAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
